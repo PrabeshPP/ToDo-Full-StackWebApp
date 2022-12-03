f34690f68560ae9aec6e330b0181e6dcdc932d9a
PrabeshPP <prabesh.bista369@gmail.com>
2022-12-03 10:33:16 -0500

    added loading spinner

---
+++ frontend/app/todos/loading.tsx
@@ -0,0 +1,9 @@
+import classes from "../../styles/loading.module.scss";
+
+export default function  Loading(){
+    return <div className={classes.box}>
+        <div className={classes.loadingSpinner}>
+
+        </div>
+    </div>
+}
